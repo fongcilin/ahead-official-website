a1449921f6c531fbedef0a8f0460ddbf6d8444a6
paul yang <b27294301@gmail.com>
2025-02-23 20:55:07 +0800

    chore: rename component

---
+++ components/custom/pink-text-block.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/try-stuff/lib/utils';
+
+interface PinkTextBlockProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export const PinkTextBlock = ({ children, className }: PinkTextBlockProps) => {
+  return (
+    <div
+      className={cn(
+        'rounded-md bg-gradient-to-b from-red-200 to-red-50 px-6 pb-20 pt-6 text-gray-500',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
